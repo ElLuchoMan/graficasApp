df4bdd0dd4b3131f565eb301debdc3d2695771d9
ElLuchoMan <luchoman96@gmail.com>
2021-02-22 20:41:58 -0500

    Agregada grafica de dona

--- src/app/graficas/pages/dona/dona.component.ts
+++ src/app/graficas/pages/dona/dona.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartType } from 'chart.js';
+import { Color, Label, MultiDataSet } from 'ng2-charts';
 
 @Component({
   selector: 'app-dona',
@@ -7,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class DonaComponent implements OnInit {
-
+  // Doughnut
+  public doughnutChartLabels: Label[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales', 'Other'];
+  public doughnutChartData: MultiDataSet = [
+    [150, 250, 350,450],
+     ];
+  public doughnutChartType: ChartType = 'doughnut';
+public colors:Color[]=[
+  {backgroundColor:['#0075ED','#00BAF7','#00E0DB','#00F7AD','#00ED63']}
+]
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  // events
+  public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
   }
 
 }
